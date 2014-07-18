31c86e627496341b6d1d73f7e90e94c8bfd5c1dc
刘雨骁 <liuyuxiao123@gmail.com>
2014-07-18 10:51:01 +0800

    Update tracefunctiongraph_longtime.sh

--- tracefunctiongraph_longtime.sh
+++ tracefunctiongraph_longtime.sh
@@ -4,13 +4,15 @@ LPATH="/sys/kernel/debug/binder"
 DATE=$(date +%Y%m%d%H%M%S)
 echo nop > $DPATH/current_tracer
 echo > $DPATH/trace
+echo > $DPATH/trace_pipe
 
 # set function tracer
 echo function_graph > $DPATH/current_tracer
 echo *binder*> $DPATH/set_ftrace_filter
 echo funcgraph-abstime > $DPATH/trace_options
-ps -t> /sdcard/binder_ftrace_ref_$DATE.txt
+ps -t > /sdcard/binder_ftrace_ref_$DATE.txt
 # start the tracing
+echo 1 > $DPATH/tracing_on
 cat $DPATH/trace_pipe > /sdcard/binder_ftrace_$DATE.txt &
 
 
